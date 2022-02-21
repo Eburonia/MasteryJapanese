adfba5795b2665a459a2d3d81753719bef0ddbf1
Eburonia <maurice.vossen@planet.nl>
2022-02-21 18:53:48 +0000

    add

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+
+
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=False)
