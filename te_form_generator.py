7732420b79bf10299d3b0ec0603ac06fe8df95d9
Eburonia <maurice.vossen@planet.nl>
2022-03-07 20:03:42 +0000

    update tenses

--- te_form_generator.py
+++ te_form_generator.py
@@ -1,57 +1,105 @@
-'''
-Te-form generator
-'''
+''' Te Form Generator '''
 
 
 from verbs import verbs
 
 
-def te_form(verb):
+def te_form(verb, ret):
 
-    ''' Te form generator '''
+    ''' Te form generator
+    ret = 0 = return Hiragana or Mazegaki
+    ret = 1 = return Mazegaki '''
 
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    type_ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
-        return verb[:-1] + 'て'
+    if type_ == 'ru':
+
+        if ret == 0:
+
+            # Return Hiragana
+            return verb[:-1] + 'て'
+
+        if ret == 1:
+
+            # Return Mazegaki
+            return verbs[verb]['hiragana'][:-1] + 'て'
+
+        else:
+
+            return 'ERROR'
 
     # When 'u' verb
-    elif verb_type == 'u':
+    elif type_ == 'u':
 
         last_syllable = verb[-1:]
 
         if last_syllable == 'う':
-            return verb[:-1] + 'って'
+
+            # Return Hiragana
+            if ret == 0:
+
+                return verb[:-1] + 'って'
+
+            # Return Mazegaki
+            if ret == 1:
+
+                return verbs[verb]['hiragana'][:-1] + 'って'
+
+            else:
+
+                return 'ERROR'
 
         elif last_syllable == 'く':
+
             return verb[:-1] + 'いて'
 
         elif last_syllable == 'す':
+
             return verb[:-1] + 'して'
 
         elif last_syllable == 'つ':
+
             return verb[:-1] + 'って'
 
         elif last_syllable == 'ぬ':
+
             return verb[:-1] + 'んで'
 
         elif last_syllable == 'む':
-            return verb[:-1] + 'んで'
+
+            # Return Hiragana
+            if ret == 0:
+
+                return verb[:-1] + 'んで'
+
+            # Return Mazegaki
+            if ret == 1:
+
+                return verbs[verb]['hiragana'][:-1] + 'んで'
+
+            else:
+
+                return 'ERROR'
 
         elif last_syllable == 'る':
+
             return verb[:-1] + 'って'
 
         elif last_syllable == 'ぐ':
+
             return verb[:-1] + 'いで'
 
         elif last_syllable == 'ぶ':
+
             return verb[:-1] + 'んで'
 
         else:
+
             return 'error'
 
     # When not 'ru' or 'u' verb
     else:
+
         return 'error'
--- tenses.py
+++ tenses.py
@@ -1,6 +1,5 @@
-'''
-    Tenses
-'''
+''' Tenses '''
+
 
 from verb_table_shifter import verb_table_shifter
 from te_form_generator import te_form
@@ -12,19 +11,38 @@ def present_polite_positive(verb):
 
     ''' Present Polite Positive '''
 
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    _ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
-        return verb[:-1] + 'ます'
+    if type == 'ru':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + 'ます'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + 'ます'
+
+        return list_
 
     # When 'u' verb
-    elif verb_type == 'u':
-        return verb[:-1] + verb_table_shifter(verb, 2) + 'ます'
+    elif type == 'u':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + verb_table_shifter(
+                                                    verb, 2) + 'ます'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + verb_table_shifter(verb, 2) + 'ます'
+
+        return list_
 
     # When not 'ru' or 'u' verb
     else:
+
         return 'error'
 
 
@@ -33,19 +51,38 @@ def present_polite_negative(verb):
 
     ''' Present Polite Negative '''
 
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    type_ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
-        return verb[:-1] + 'ません'
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + 'ません'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + 'ません'
+
+        return list_
 
     # When 'u' verb
-    elif verb_type == 'u':
-        return verb[:-1] + verb_table_shifter(verb, 2) + 'ません'
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + verb_table_shifter(
+                                                    verb, 2) + 'ません'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + verb_table_shifter(verb, 2) + 'ません'
+
+        return list_
 
     # When not 'ru' or 'u' verb
     else:
+
         return 'error'
 
 
@@ -54,19 +91,38 @@ def past_polite_positive(verb):
 
     ''' Past Polite Positive '''
 
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    type_ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
-        return verb[:-1] + 'ました'
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + 'ました'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + 'ました'
+
+        return list_
 
     # When 'u' verb
-    elif verb_type == 'u':
-        return verb[:-1] + verb_table_shifter(verb, 2) + 'ました'
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + verb_table_shifter(
+                                                    verb, 2) + 'ました'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + verb_table_shifter(verb, 2) + 'ました'
+
+        return list_
 
     # When not 'ru' or 'u' verb
     else:
+
         return 'error'
 
 
@@ -75,19 +131,38 @@ def past_polite_negative(verb):
 
     ''' Past Polite Negative '''
 
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    _ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
-        return verb[:-1] + 'ませんでした'
+    if type == 'ru':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + 'ませんでした'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + 'ませんでした'
+
+        return list_
 
     # When 'u' verb
-    elif verb_type == 'u':
-        return verb[:-1] + verb_table_shifter(verb, 2) + 'ませんでした'
+    elif type == 'u':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + verb_table_shifter(
+                                                verb, 2) + 'ませんでした'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + verb_table_shifter(verb, 2) + 'ませんでした'
+
+        return list_
 
     # When not 'ru' or 'u' verb
     else:
+
         return 'error'
 
 
@@ -96,7 +171,16 @@ def present_plain_positive(verb):
 
     ''' Present Plain Positive '''
 
-    return verb
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
+    # Return Hiragana
+    list_[0] = verbs[verb]['hiragana']
+
+    # Return Mazegaki (Kanji + Hiragana)
+    list_[1] = verb
+
+    return list_
 
 
 # Present Plain Negative
@@ -104,19 +188,38 @@ def present_plain_negative(verb):
 
     ''' Present Plain Negative '''
 
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    type_ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
-        return verb[:-1] + 'ない'
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + 'ない'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + 'ない'
+
+        return list_
 
     # When 'u' verb
-    elif verb_type == 'u':
-        return verb[:-1] + verb_table_shifter(verb, 1) + 'ない'
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = verb[:-1] + verb_table_shifter(verb, 1) + 'ない'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verbs[verb]['hiragana'][:-1] + verb_table_shifter(
+                                                    verb, 1) + 'ない'
+
+        return list_
 
     # When not 'ru' or 'u' verb
     else:
+
         return 'error'
 
 
@@ -125,27 +228,46 @@ def past_plain_positive(verb):
 
     ''' Past Plain Positive '''
 
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    type_ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
+    if type_ == 'ru':
 
-        return te_form(verb)[:-1] + 'た'
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + 'た'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0)[:-1] + 'た'
+
+        return list_
 
     # When 'u' verb
-    elif verb_type == 'u':
+    elif type_ == 'u':
 
-        if te_form(verb)[-1:] == 'て':
-            return te_form(verb)[:-1] + 'た'
+        if te_form(verb, 0)[-1:] == 'て':
 
-        elif te_form(verb)[-1:] == 'で':
-            return te_form(verb)[:-1] + 'だ'
+            list_[0] = te_form(verb, 0)[:-1] + 'た'
+            list_[1] = te_form(verb, 1)[:-1] + 'た'
+
+            return list_
+
+        elif te_form(verb, 0)[-1:] == 'で':
+
+            list_[0] = te_form(verb, 0)[:-1] + 'だ'
+            list_[1] = te_form(verb, 1)[:-1] + 'だ'
+
+            return list_
 
         else:
+
             return 'error'
 
     else:
+
         return 'error'
 
 
@@ -154,17 +276,188 @@ def past_plain_negative(verb):
 
     ''' Past Plain Negative '''
 
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
     # Get the verb type (ru/u)
-    verb_type = verbs[verb]['type']
+    type_ = verbs[verb]['type']
 
     # When 'ru' verb
-    if verb_type == 'ru':
-        return verb[:-1] + 'なかった'
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = verbs[verb]['hiragana'][:-1] + 'なかった'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verb[:-1] + 'なかった'
+
+        return list_
 
     # When 'u' verb
-    elif verb_type == 'u':
-        return verb[:-1] + verb_table_shifter(verb, 1) + 'なかった'
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = verb[:-1] + verb_table_shifter(verb, 1) + 'なかった'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = verbs[verb]['hiragana'][:-1] + verb_table_shifter(
+                                                    verb, 1) + 'なかった'
+
+        return list_
 
     # When not 'ru' or 'u' verb
     else:
+
+        return 'error'
+
+
+def present_polite_progressive_positive(verb):
+
+    ''' Present Polite Progressive Positive '''
+
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
+    # Get the verb type (ru/u)
+    type_ = verbs[verb]['type']
+
+    # When 'ru' verb
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'います'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'います'
+
+        return list_
+
+    # When 'u' verb
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'います'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'います'
+
+        return list_
+
+    # When not 'ru' or 'u' verb
+    else:
+
+        return 'error'
+
+
+def present_polite_progressive_negative(verb):
+
+    ''' Present Polite Progressive Negative '''
+
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
+    # Get the verb type (ru/u)
+    type_ = verbs[verb]['type']
+
+    # When 'ru' verb
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'います'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'います'
+
+        return list_
+
+    # When 'u' verb
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'いません'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'いません'
+
+        return list_
+
+    # When not 'ru' or 'u' verb
+    else:
+
+        return 'error'
+
+
+def past_polite_progressive_positive(verb):
+
+    ''' Past Polite Progressive Positive '''
+
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
+    # Get the verb type (ru/u)
+    type_ = verbs[verb]['type']
+
+    # When 'ru' verb
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'いました'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'いました'
+
+        return list_
+
+    # When 'u' verb
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'いました'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'いました'
+
+        return list_
+
+    # When not 'ru' or 'u' verb
+    else:
+
+        return 'error'
+
+
+def past_polite_progressive_negative(verb):
+
+    ''' Past Polite Progressive Negative '''
+
+    # Declare Hiragana and Mazegaki
+    list_ = ['', '']
+
+    # Get the verb type (ru/u)
+    type_ = verbs[verb]['type']
+
+    # When 'ru' verb
+    if type_ == 'ru':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'いませんでした'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'いませんでした'
+
+        return list_
+
+    # When 'u' verb
+    elif type_ == 'u':
+
+        # Return Hiragana
+        list_[0] = te_form(verb, 1) + 'いませんでした'
+
+        # Return Mazegaki (Kanji + Hiragana)
+        list_[1] = te_form(verb, 0) + 'いませんでした'
+
+        return list_
+
+    # When not 'ru' or 'u' verb
+    else:
+
         return 'error'
