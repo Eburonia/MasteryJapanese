27540d7fbf16f79a9373974886d62b7c020cfd76
Eburonia <maurice.vossen@planet.nl>
2022-03-18 20:56:29 +0000

    update

--- app.py
+++ app.py
@@ -267,4 +267,4 @@ def reset_session_memory():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
